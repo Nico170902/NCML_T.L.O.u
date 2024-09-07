432b3d26ec61378dd3482bf142f25679436a94e1
Nico170902 <calle@Laptop_Nico09>
2024-09-07 12:10:19 +0200

    secondo

--- src/main/java/net/mcreator/ncmllof/procedures/NoostilemobProcedure.java
+++ src/main/java/net/mcreator/ncmllof/procedures/NoostilemobProcedure.java
@@ -6,11 +6,18 @@ import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.monster.Witch;
 import net.minecraft.world.entity.monster.EnderMan;
 import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.entity.MobType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.tags.TagKey;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.CommandSource;
 
 import net.mcreator.ncmllof.NcmllofMod;
 
@@ -30,7 +37,15 @@ public class NoostilemobProcedure {
 	private static void execute(@Nullable Event event, LevelAccessor world, Entity entity) {
 		if (entity == null)
 			return;
-		if (entity instanceof LivingEntity _livEnt0 && _livEnt0.getMobType() == MobType.UNDEAD || entity instanceof LivingEntity _livEnt1 && _livEnt1.getMobType() == MobType.ARTHROPOD || entity instanceof Creeper || entity instanceof EnderMan) {
+		if (entity.getType().is(TagKey.create(Registries.ENTITY_TYPE, new ResourceLocation("minecraft:skeletons"))) || entity instanceof LivingEntity _livEnt1 && _livEnt1.getMobType() == MobType.ARTHROPOD || entity instanceof Creeper
+				|| entity instanceof Witch || entity instanceof EnderMan) {
+			{
+				Entity _ent = entity;
+				if (!_ent.level().isClientSide() && _ent.getServer() != null) {
+					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4,
+							_ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "summon zombie");
+				}
+			}
 			NcmllofMod.queueServerWork(2, () -> {
 				if (!entity.level().isClientSide())
 					entity.discard();
